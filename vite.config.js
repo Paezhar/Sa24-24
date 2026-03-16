f3d0e08c4dee0689d8338e94984f4f0bf0740e78
Paezhar <hugo_bou89@hotmail.ca>
2026-03-16 14:35:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
-import { default as react } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
 })
-```
-
-**Commit changes.**
-
-Ensuite vérifie que ton repo contient aussi `index.html` à la racine — c'est requis par Vite. S'il n'est pas là, crée-le aussi :
-
-Nomme le fichier :
-```
-index.html
